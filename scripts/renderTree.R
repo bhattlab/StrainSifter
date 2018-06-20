86fc08deaa781b207bb1f0e813b61acd1797ea25
tamburinif <tamburin@stanford.edu>
2018-06-19 18:09:25 -0700

    update tutorial

--- scripts/renderTree.R
+++ scripts/renderTree.R
@@ -7,7 +7,7 @@ tree_file <- snakemake@input[[1]]
 out_file <- snakemake@output[[1]]
 
 # read tree file
-tree <- read.tree(tree.file)
+tree <- read.tree(tree_file)
 
 # midpoint root tree if there is more than 1 node
 if (tree$Nnode > 1){
@@ -15,11 +15,11 @@ if (tree$Nnode > 1){
 }
 
 # draw tree
-p <- ggtree(tree) +
-  geom_tiplab(offset = .005) +
+p <- ggtree(tree, branch.length = 1) +
+  geom_tiplab(offset = .05) +
   geom_tippoint(size=3) +
-  xlim(NA, 1) +
-  geom_treescale(width = 0.1)
+  xlim(0, 4) +
+  geom_treescale(width = 0.1, offset=0.25)
 
 # save output file
-ggsave(out_file, plot = p, height = .5*length(tree$tip.label))
+ggsave(out_file, plot = p, height = 1*length(tree$tip.label))
